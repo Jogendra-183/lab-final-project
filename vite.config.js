dd441e9e4e60ec1b3654f7876f264c651658461c
Jogendra-183 <jogendrasai780@gmail.com>
2025-11-07 10:06:44 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
- import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// IMPORTANT: base must match the repo name for GH Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/artconnect1/'
+  base: "./",
 });
